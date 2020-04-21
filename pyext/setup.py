d72c20ed90a23bc3a5bab80092742f5e5d51c0cd
David Huggins-Daines <dhdaines@gmail.com>
2020-04-20 20:37:05 -0400

    use scikit-build to build the python package, makes a working wheel but otherwise does not quite work yet!

---
+++ pyext/setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="soundswallower",
+    version="0.1",
+    description="An even smaller speech recognizer",
+    author="David Huggins-Daines",
+    license="MIT",
+    packages=["soundswallower"]
+    )
