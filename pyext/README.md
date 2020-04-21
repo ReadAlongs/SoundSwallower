9a9f6232f8ec218cfc15bbe88cfae81d223b6cbb
David Huggins-Daines <dhdaines@gmail.com>
2020-04-21 19:31:07 -0400

    move the Python build to the top level (might do this with JavaScript too)

--- pyext/README.md
+++ pyext/README.md
@@ -6,3 +6,6 @@ the SWIG wrappers because sometimes it is a pain to get SWIG to
 install, but if you wish to regenerate them, you can run:
 
 swig -o soundswallower_wrap.c -outdir soundswallower -python ../swig/soundswallower.i
+
+To compile the extension, run 'python setup.py build' from the top
+level directory.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from skbuild import setup
+
+setup(
+    name="soundswallower",
+    license="MIT",
+    version="0.1.1",
+    description="An even smaller speech recognizer",
+    author="David Huggins-Daines",
+    author_email="dhdaines@gmail.com"
+    url="https://github.com/ReadAlongs/SoundSwallower",
+    packages=["soundswallower"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: C",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
