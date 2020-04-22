756b9c70eea6e1906e92dc2e0906a9a1dde04bc2
David Huggins-Daines <dhdaines@gmail.com>
2020-04-21 21:11:48 -0400

    add get_model_path

--- soundswallower/__init__.py
+++ soundswallower/__init__.py
@@ -1,2 +1,10 @@
+import os
+
 from .soundswallower import *
 
+DefaultConfig = Decoder.default_config
+
+
+def get_model_path():
+    """ Return path to the model. """
+    return os.path.join(os.path.dirname(__file__), 'model')
