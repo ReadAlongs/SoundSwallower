85a1b29096c8dddc3ff89cd13f03f4aa65f9a5d7
David Huggins-Daines <dhdaines@gmail.com>
2021-01-16 18:24:08 -0500

    start rebuilding the tests from zero

---
+++ py/test/test_decoder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+import unittest
+from soundswallower import Decoder, get_model_path
+
+
+DATADIR = os.path.join(os.path.dirname(__file__),
+                       "..", "..", "tests", "data")
+
+
+class TestDecoder(unittest.TestCase):
+    def test_turtle(self):
+        config = Decoder.default_config()
+        config['-hmm'] = os.path.join(get_model_path(), 'en-us')
+        config['-fsg'] = os.path.join(DATADIR, 'goforward.fsg')
+        config['-dict'] = os.path.join(DATADIR, 'turtle.dic')
+        config['-logfn'] = '/dev/null'
+        decoder = Decoder(config)
+        with open(os.path.join(DATADIR, 'goforward.raw'), 'rb') as fh:
+            buf = fh.read()
+            decoder.start_utt()
+            decoder.process_raw(buf, full_utt=True)
+            decoder.end_utt()
+            self.assertEqual(decoder.hyp().hypstr, "go forward ten meters")
+
