fec7846a5258c1ebbf3cd330e1d8cd5483cb9128
David Huggins-Daines <dhdaines@gmail.com>
2022-04-26 10:53:45 -0400

    update code sizes

--- js/soundswallower.c
+++ js/soundswallower.c
@@ -3,8 +3,8 @@
  *
  * This currently contains nothing.
  *
- * C: -rw-rw-r-- 1 dhd dhd  75683 avr 26 09:33 soundswallower.js
- * -rwxrwxr-x 1 dhd dhd 204713 avr 26 09:33 soundswallower.wasm
+ * C:   -rw-rw-r-- 1 dhd dhd  77246 avr 26 10:51 soundswallower.js
+  -rwxrwxr-x 1 dhd dhd 204703 avr 26 10:51 soundswallower.wasm
  *
  * C++: -rw-rw-r-- 1 dhd dhd 119254 avr 26 09:34 soundswallower.js
 -rwxrwxr-x 1 dhd dhd 231829 avr 26 09:34 soundswallower.wasm
