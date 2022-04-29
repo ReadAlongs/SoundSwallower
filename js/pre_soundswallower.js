480cdfe69396e1e28c9d57fc5ce1f7b0f0a52481
David Huggins-Daines <dhdaines@gmail.com>
2022-04-28 20:49:08 -0400

    switch wrappers to call functions using Module instead of this, to avoid confusion

--- js/pre_soundswallower.js
+++ js/pre_soundswallower.js
@@ -14,38 +14,39 @@ Module.cmd_ln_type = cwrap('cmd_ln_type_r', 'number',
 			   ['number', 'string']);
 Module.cmd_ln_set = function(cmd_ln, key, val) {
     let ckey = allocateUTF8OnStack(key);
-    let type = this._cmd_ln_type_r(cmd_ln, ckey);
+    let type = Module._cmd_ln_type_r(cmd_ln, ckey);
     if (type == 0) {
 	throw new ReferenceError("Unknown cmd_ln parameter "+key);
     }
     if (type & ARG_STRING) {
 	let cval = allocateUTF8OnStack(val);
-	this._cmd_ln_set_str_r(cmd_ln, ckey, cval);
+	Module._cmd_ln_set_str_r(cmd_ln, ckey, cval);
     }
     else if (type & ARG_FLOATING) {
-	this._cmd_ln_set_float_r(cmd_ln, ckey, val);
+	Module._cmd_ln_set_float_r(cmd_ln, ckey, val);
     }
     else if (type & (ARG_INTEGER | ARG_BOOLEAN)) {
-	this._cmd_ln_set_int_r(cmd_ln, ckey, val);
+	Module._cmd_ln_set_int_r(cmd_ln, ckey, val);
     }
     else {
-	throw new TypeError("Unsupported type "+type+"for parameter"+key);
+	return false;
     }
+    return true;
 };
 Module.cmd_ln_get = function(cmd_ln, key) {
     let ckey = allocateUTF8OnStack(key);
-    let type = this._cmd_ln_type_r(cmd_ln, ckey);
+    let type = Module._cmd_ln_type_r(cmd_ln, ckey);
     if (type == 0) {
 	throw new ReferenceError("Unknown cmd_ln parameter "+key);
     }
     if (type & ARG_STRING) {
-	return this._cmd_ln_str_r(cmd_ln, ckey);
+	return Module._cmd_ln_str_r(cmd_ln, ckey);
     }
     else if (type & ARG_FLOATING) {
-	return this._cmd_ln_float_r(cmd_ln, ckey);
+	return Module._cmd_ln_float_r(cmd_ln, ckey);
     }
     else if (type & (ARG_INTEGER | ARG_BOOLEAN)) {
-	return this._cmd_ln_int_r(cmd_ln, ckey);
+	return Module._cmd_ln_int_r(cmd_ln, ckey);
     }
     else {
 	throw new TypeError("Unsupported type "+type+" for parameter"+key);
@@ -55,13 +56,13 @@ Module.ps_process_raw = cwrap('ps_process_raw', 'number',
 			      ['number', 'array', 'number', 'number', 'number']);
 Module.ps_get_hyp = cwrap('ps_get_hyp', 'string', ['number', 'number']);
 Module.ps_get_hypseg = function(decoder) {
-    let itor = this._ps_seg_iter(decoder);
-    let config = this._ps_get_config(decoder);
-    let frate = this._cmd_ln_int_r(config, allocateUTF8OnStack("-frate"));
+    let itor = Module._ps_seg_iter(decoder);
+    let config = Module._ps_get_config(decoder);
+    let frate = Module._cmd_ln_int_r(config, allocateUTF8OnStack("-frate"));
     let seg = [];
     while (itor != 0) {
 	let frames = stackAlloc(8);
-	this._ps_seg_frames(itor, frames, frames + 4);
+	Module._ps_seg_frames(itor, frames, frames + 4);
 	let start_frame = getValue(frames, 'i32');
 	let end_frame = getValue(frames + 4, 'i32');
 	seg_item = {
@@ -70,16 +71,16 @@ Module.ps_get_hypseg = function(decoder) {
 	    end: end_frame / frate
 	};
 	seg.push(seg_item);
-	itor = this._ps_seg_next(itor);
+	itor = Module._ps_seg_next(itor);
     }
     return seg;
 };
 Module.ps_add_word = cwrap('ps_add_word', 'number',
 			   ['number', 'string', 'string', 'number']);
 Module.ps_create_fsg = function(decoder, name, start_state, final_state, transitions) {
-    let logmath = this._ps_get_logmath(decoder);
-    let config = this._ps_get_config(decoder);
-    let lw = this._cmd_ln_float_r(config, allocateUTF8OnStack("-lw"));
+    let logmath = Module._ps_get_logmath(decoder);
+    let config = Module._ps_get_config(decoder);
+    let lw = Module._cmd_ln_float_r(config, allocateUTF8OnStack("-lw"));
     let n_state = 0;
     for (let t of transitions) {
 	n_state = Math.max(n_state, t.from, t.to);
@@ -88,30 +89,30 @@ Module.ps_create_fsg = function(decoder, name, start_state, final_state, transit
     let fsg = ccall('fsg_model_init',
 		    'number', ['string', 'number', 'number', 'number'],
 		    [name, logmath, lw, n_state]);
-    this._fsg_set_states(fsg, start_state, final_state);
+    Module._fsg_set_states(fsg, start_state, final_state);
     for (let t of transitions) {
 	let logprob = 0;
 	if ('prob' in t) {
-	    logprob = this._logmath_log(logmath, t.prob);
+	    logprob = Module._logmath_log(logmath, t.prob);
 	}
 	if ('word' in t) {
 	    let wid = ccall('fsg_model_word_add', 'number',
 			    ['number', 'string'],
 			    [fsg, t.word]);
 	    if (wid == -1) {
-		this._fsg_model_free(fsg);
+		Module._fsg_model_free(fsg);
 		return 0;
 	    }
-	    this._fsg_model_trans_add(fsg, t.from, t.to, logprob, wid);
+	    Module._fsg_model_trans_add(fsg, t.from, t.to, logprob, wid);
 	}
 	else {
-	    this._fsg_model_null_trans_add(fsg, t.from, t.to, logprob);
+	    Module._fsg_model_null_trans_add(fsg, t.from, t.to, logprob);
 	}
     }
     return fsg;
 };
 Module.ps_set_fsg = function(decoder, fsg) {
-    if (this._ps_set_fsg(decoder, "_default", fsg) != 0) {
+    if (Module._ps_set_fsg(decoder, "_default", fsg) != 0) {
 	throw new Error("Failed to set FSG in decoder");
     }
 };
