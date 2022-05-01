923d39b0f98d2fcbffb0ae3214088b0e229f31ac
David Huggins-Daines <dhdaines@gmail.com>
2022-05-01 08:29:09 -0400

    clean up test code

--- js/test_simple.js
+++ js/test_simple.js
@@ -1,37 +1,49 @@
 #!/usr/bin/env node
 
-// Monkey-patch this so that preloading files will work (see
-// https://github.com/emscripten-core/emscripten/issues/16742)
+// Note that we could use NODERAWFS but the module would have to be
+// recompiled for the browser.  So, we monkey-patch this so that
+// preloading files will work (see
+// https://github.com/emscripten-core/emscripten/issues/16742).  Note
+// also that we cannot use lazy loading as it corrupts binary files
+// (see https://github.com/emscripten-core/emscripten/issues/16740)
 Browser = {
     handledByPreloadPlugin() {
 	return false;
     }
 };
 
-// Cannot use lazy loading as it corrupts binary files (see
-// https://github.com/emscripten-core/emscripten/issues/16740)
-function preload_models() {
-    Module.FS_createPath("/", "en-us", true, true);
-    Module.FS_createPreloadedFile("/", "en-us.dict", "../model/en-us.dict", true, true);
-    Module.FS_createPreloadedFile("/", "goforward.fsg", "../tests/data/goforward.fsg", true, true);
-    Module.FS_createPreloadedFile("/en-us", "transition_matrices", "../model/en-us/transition_matrices", true, true);
-    Module.FS_createPreloadedFile("/en-us", "feat.params", "../model/en-us/feat.params", true, true);
-    Module.FS_createPreloadedFile("/en-us", "mdef", "../model/en-us/mdef", true, true);
-    Module.FS_createPreloadedFile("/en-us", "means", "../model/en-us/means", true, true);
-    Module.FS_createPreloadedFile("/en-us", "noisedict", "../model/en-us/noisedict", true, true);
-    Module.FS_createPreloadedFile("/en-us", "sendump", "../model/en-us/sendump", true, true);
-    Module.FS_createPreloadedFile("/en-us", "variances", "../model/en-us/variances", true, true);
-}
-Module = {
-    preRun: preload_models
+// Emscripten will use this as the Module object inside ssjs.
+// Unfortunately preRun() is not called as a method, so we have to bind
+// the object to a name here in order to access the Emscripten runtime.
+var modinit = {
+    preRun() {
+	modinit.FS_createPath("/", "en-us", true, true);
+	modinit.FS_createPreloadedFile("/", "en-us.dict", "../model/en-us.dict",
+				      true, true);
+	modinit.FS_createPreloadedFile("/", "goforward.fsg",
+				      "../tests/data/goforward.fsg", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "transition_matrices",
+				      "../model/en-us/transition_matrices", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "feat.params",
+				      "../model/en-us/feat.params", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "mdef",
+				      "../model/en-us/mdef", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "means",
+				      "../model/en-us/means", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "noisedict",
+				      "../model/en-us/noisedict", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "sendump",
+				      "../model/en-us/sendump", true, true);
+	modinit.FS_createPreloadedFile("/en-us", "variances",
+				      "../model/en-us/variances", true, true);
+    }
 };
-
-var ssjs; // Loaded below
-const fs = require('fs/promises');
 
 // Wrap in async function so we can wait for WASM load
 (async () => {
-    ssjs = await require('./js/soundswallower.js')(Module);
+    const fs = await require('fs/promises');
+    // Note that ssjs and modinit are (probably) the same object after this
+    var ssjs = await require('./js/soundswallower.js')(modinit);
     let decoder = new ssjs.Decoder({
 	hmm: "en-us",
 	dict: "en-us.dict",
@@ -54,6 +66,7 @@ const fs = require('fs/promises');
 	{from: 3, to: 4, prob: 1.0, word: "_meters"}
     ]);
     decoder.set_fsg(fsg);
+    decoder.config.set("-backtrace", true);
     fsg.delete() // has been retained by decoder
     decoder.start();
     decoder.process_raw(pcm, false, true);
