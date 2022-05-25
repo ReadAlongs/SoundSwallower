b4542b8486e8aee5769c9a1ad8e47913c469e88c
David Huggins-Daines <dhdaines@gmail.com>
2022-05-25 17:40:39 -0400

    adjust versions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="soundswallower",
     license="MIT",
-    version="0.2.1",
+    version="0.2.2",
     description="An even smaller speech recognizer",
     long_description=long_description,
     long_description_content_type="text/markdown",
