9b196ef5fa6923a5919cf633e4fb3f9f101029ad
David Huggins-Daines <dhdaines@gmail.com>
2022-06-15 09:07:16 -0400

    build: be more standards-compliant

--- setup.py
+++ setup.py
@@ -1,32 +1,9 @@
 from skbuild import setup
 from setuptools import find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
-    name="soundswallower",
-    license="MIT",
-    version="0.2.3-alpha0",
-    description="An even smaller speech recognizer",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="David Huggins-Daines",
-    author_email="dhdaines@gmail.com",
-    url="https://github.com/ReadAlongs/SoundSwallower",
-    project_urls={
-        "Documentation": "https://soundswallower.readthedocs.io/"
-    },
     packages=find_packages('py', exclude=["test"]),
     package_dir={"": "py"},
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: C",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     entry_points={
         "console_scripts": [
             "soundswallower = soundswallower.cli:main",
