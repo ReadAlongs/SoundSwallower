96624e431dfcf6fc2c5b61eba519a43ba6247717
David Huggins-Daines <dhdaines@gmail.com>
2022-06-17 08:59:43 -0400

    refactor: proper code formatting

--- js/soundswallower.c
+++ js/soundswallower.c
@@ -1,3 +1,4 @@
+/* -*- c-basic-offset: 4; c-file-style: "bsd" -*- */
 /**
  * soundswallower.c: Extra C code specific to SoundSwallower in JavaScript
  */
@@ -10,22 +11,22 @@
 EMSCRIPTEN_KEEPALIVE void
 fsg_set_states(fsg_model_t *fsg, int start_state, int final_state)
 {
-  fsg->start_state = start_state;
-  fsg->final_state = final_state;
+    fsg->start_state = start_state;
+    fsg->final_state = final_state;
 }
 
 EMSCRIPTEN_KEEPALIVE hash_iter_t *
 cmd_ln_hash_iter(cmd_ln_t *cmd_ln)
 {
-  if (cmd_ln == NULL || cmd_ln->ht == NULL)
-    return NULL;
-  return hash_table_iter(cmd_ln->ht);
+    if (cmd_ln == NULL || cmd_ln->ht == NULL)
+	return NULL;
+    return hash_table_iter(cmd_ln->ht);
 }
 
 EMSCRIPTEN_KEEPALIVE char const *
 hash_iter_key(hash_iter_t *itor)
 {
-  if (itor == NULL || itor->ent == NULL)
-    return NULL;
-  return hash_entry_key(itor->ent);
+    if (itor == NULL || itor->ent == NULL)
+	return NULL;
+    return hash_entry_key(itor->ent);
 }
