709c6a52d7850f3099d95fccac9e3190b33d472d
David Huggins-Daines <dhdaines@gmail.com>
2022-06-20 23:57:33 -0400

    refactor: fix style

--- include/soundswallower/ms_gauden.h
+++ include/soundswallower/ms_gauden.h
@@ -99,11 +99,11 @@ typedef struct gauden_s {
  * Return value: ptr to the model created; NULL if error.
  * (See Sphinx3 model file-format documentation.)
  */
-gauden_t *gauden_init (char const *meanfile,/**< Input: File containing means of mixture gaussians */
-                       char const *varfile,/**< Input: File containing variances of mixture gaussians */
-                       float32 varfloor,	/**< Input: Floor value to be applied to variances */
-                       logmath_t *lmath
-                       );
+gauden_t *gauden_init(char const *meanfile,/**< Input: File containing means of mixture gaussians */
+                      char const *varfile,/**< Input: File containing variances of mixture gaussians */
+                      float32 varfloor,	/**< Input: Floor value to be applied to variances */
+                      logmath_t *lmath
+                      );
 
 /**
  * Read mixture gaussian codebooks from s3file_t.
