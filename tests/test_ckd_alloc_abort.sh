a4e2508771068f0e276e7ee0fb0237202438a0b2
David Huggins-Daines <dhdaines@gmail.com>
2022-07-07 09:55:12 -0400

    test: enable (some) script tests as in PocketSphinx

---
+++ tests/test_ckd_alloc_abort.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+: ${CMAKE_BINARY_DIR:=$(pwd)}
+. ${CMAKE_BINARY_DIR}/tests/testfuncs.sh
+
+ulimit -c 0
+if $CMAKE_BINARY_DIR/test_ckd_alloc_abort; then
+    fail expected_failure
+else
+    pass expected_failure
+fi
+
