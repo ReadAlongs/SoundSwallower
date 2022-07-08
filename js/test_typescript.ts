aaf8ebdae7dc0a2aa6fe039a4935be642febd87e
David Huggins-Daines <dhdaines@gmail.com>
2022-07-08 11:56:04 -0400

    test: make sure fsg and jsgf work in typescript too

--- js/test_typescript.ts
+++ js/test_typescript.ts
@@ -12,6 +12,7 @@ import * as assert from "assert";
         ++nkeys;
     }
     console.log(`Found ${nkeys} keys in config`);
+    /* Basic test */
     const decoder = new soundswallower.Decoder({
 	fsg: "testdata/goforward.fsg",
 	samprate: 16000,
@@ -23,7 +24,7 @@ import * as assert from "assert";
     await decoder.stop();
     let hyp = decoder.get_hyp();
     console.log(`recognized: ${hyp}`);
-    assert.equal("go forward ten meters", decoder.get_hyp());
+    assert.equal("go forward ten meters", hyp);
     let hypseg = decoder.get_hypseg();
     let hypseg_words = []
     for (const seg of hypseg) {
@@ -33,5 +34,49 @@ import * as assert from "assert";
     }
     assert.deepStrictEqual(hypseg_words,
 			   ["go", "forward", "ten", "meters"]);
+
+    /* Test create_fsg */
+    await decoder.add_word("_go", "G OW", false);
+    await decoder.add_word("_forward", "F AO R W ER D", false);
+    await decoder.add_word("_ten", "T EH N", false);
+    await decoder.add_word("_meters", "M IY T ER Z", true);
+    let fsg = decoder.create_fsg("goforward", 0, 4, [
+	{from: 0, to: 1, prob: 1.0, word: "_go"},
+	{from: 1, to: 2, prob: 1.0, word: "_forward"},
+	{from: 2, to: 3, prob: 1.0, word: "_ten"},
+	{from: 3, to: 4, prob: 1.0, word: "_meters"}
+    ]);
+    await decoder.set_fsg(fsg);
+    fsg.delete(); // has been retained by decoder
+    pcm = await fs.readFile("testdata/goforward-float32.raw");
+    await decoder.start();
+    await decoder.process(pcm, false, true);
+    await decoder.stop();
+    hyp = decoder.get_hyp();
+    console.log(`recognized: ${hyp}`);
+    assert.equal("_go _forward _ten _meters", hyp);
+
+    /* Test JSGF */
+    fsg = decoder.parse_jsgf(`#JSGF V1.0;
+grammar pizza;
+public <order> = [<greeting>] [<want>] [<quantity>] [<size>] [<style>]
+       [(pizza | pizzas)] [<toppings>];
+<greeting> = hi | hello | yo | howdy;
+<want> = i want | gimme | give me | i'd like to order | order | i wanna;
+<quantity> = a | one | two | three | four | five;
+<size> = small | medium | large | extra large | x large | x l;
+<style> = hawaiian | veggie | vegetarian | margarita | meat lover's | all dressed;
+<toppings> = [with] <topping> ([and] <topping>)*;
+<topping> = pepperoni | ham | olives | mushrooms | tomatoes | (green | hot) peppers | pineapple;
+`);
+    await decoder.set_fsg(fsg);
+    fsg.delete();
+    pcm = await fs.readFile("testdata/pizza-float32.raw");
+    await decoder.start();
+    await decoder.process(pcm, false, true);
+    await decoder.stop();
+    hyp = decoder.get_hyp();
+    console.log(`recognized: ${hyp}`);
+    assert.equal("yo gimme four large all dressed pizzas", hyp);
     decoder.delete();
 })();
