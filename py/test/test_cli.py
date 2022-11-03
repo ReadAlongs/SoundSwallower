20b4eb782b2f0dbe015725a43ca119c036d51540
David Huggins-Daines <dhdaines@gmail.com>
2022-11-03 10:39:05 -0400

    test: slightly more substantial json output test

--- py/test/test_cli.py
+++ py/test/test_cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import re
 import json
 import unittest
 from soundswallower import cli, get_model_path
@@ -10,11 +11,21 @@ DATADIR = os.path.join(os.path.dirname(__file__),
                        "..", "..", "tests", "data")
 
 
+def baseword(w):
+    return re.sub(r"\(\d+\)$", "", w["t"])
+
+
 class TestCLI(unittest.TestCase):
-    def check_output(self, jpath):
+    def check_output(self, jpath, text="go forward ten meters"):
         with open(jpath, "rt") as infh:
             for line in infh:
-                self.assertTrue(json.loads(line))
+                result = json.loads(line)
+                self.assertTrue(result)
+                self.assertEqual(result["t"], text)
+                for word, ref in zip((w for w in result["w"]
+                                      if w["t"] != "<sil>"),
+                                     text.split()):
+                    self.assertEqual(baseword(word), ref)
 
     def test_cli_basic(self):
         with TemporaryDirectory() as tmpdir:
@@ -33,7 +44,7 @@ class TestCLI(unittest.TestCase):
                       "--output", jpath,
                       os.path.join(DATADIR, "goforward_fr.wav"),
                       os.path.join(DATADIR, "goforward_fr.raw")])
-            self.check_output(jpath)
+            self.check_output(jpath, "avance de dix mètres")
 
     def test_cli_config(self):
         with TemporaryDirectory() as tmpdir:
