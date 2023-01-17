7df8739c87e505bf4a56a2e46f4d772506e53e65
David Huggins-Daines <dhdaines@gmail.com>
2023-01-17 08:11:43 -0500

    build: include type hints

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages
 setup(
     packages=find_packages('py', exclude=["test"]),
     package_dir={"": "py"},
+    package_data={"soundswallower": ["py.typed", "_soundswallower.pyi"]},
     entry_points={
         "console_scripts": [
             "soundswallower = soundswallower.cli:main",
